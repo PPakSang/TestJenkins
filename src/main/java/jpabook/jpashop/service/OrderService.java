e15de3b0a424276226dbe9d19a70ef106cb6f9df
PPakSang <ppm5377@naver.com>
2022-07-20 11:23:29 +0900

    feat 테스트기능1을 추가하였습니다

--- src/main/java/jpabook/jpashop/service/OrderService.java
+++ src/main/java/jpabook/jpashop/service/OrderService.java
@@ -54,7 +54,7 @@ public class OrderService {
     }
 
     //검색
-//    public List<Order> findOrders(OrderSearch orderSearch) {
-//        return orderRepository.findAll(orderSearch);
-//    }
+    public List<Order> findOrders(OrderSearch orderSearch) {
+        return orderRepository.findAll(orderSearch);
+    }
 }
