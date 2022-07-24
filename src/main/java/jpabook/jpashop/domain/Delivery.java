6f5b804919ba1b337fbe8193fa005833802b2b57
PPakSang <ppm5377@naver.com>
2022-07-24 11:01:53 +0900

    복구

--- src/main/java/jpabook/jpashop/domain/Delivery.java
+++ src/main/java/jpabook/jpashop/domain/Delivery.java
@@ -16,7 +16,7 @@ public class Delivery {
     @OneToOne(mappedBy = "delivery" ,fetch = FetchType.LAZY)
     private Order order;
 
-    private String TestItem;
+    private String LeeJaeHan;
 
     private Address address;
 
